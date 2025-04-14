126f6ad444dd2fb059814634c0c947e4898f0075
Dmitriy NetoIT <pr0100_smile@mail.ru>
2025-04-14 22:08:56 +0300

    Dynamic JS

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import "./css/style.css";
 
 import "./js/app";
 
-// TODO: write your code in app.js
+const gridContainer = document.querySelector('.grid-rows');
+        
+for (let i = 0; i < 4; i++) {
+    const row = document.createElement('div');
+    row.classList.add('grid-row');
+    
+    for (let j = 0; j < 4; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+        row.appendChild(cell);
+    }
+    
+    gridContainer.appendChild(row);
+}
